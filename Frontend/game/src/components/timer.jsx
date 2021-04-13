a943b52c6d739d31d1238fdff9d5e0aaf605a60b
Akhenaten <gamena750@abv.bg>
2021-04-13 23:31:41 +0100

    Timer and secconds componnet done just needs testing whether it works with the rest of the apllication

---
+++ Frontend/game/src/components/timer.jsx
@@ -0,0 +1,82 @@
+import React, {Component} from 'react'
+import SecondsCounter from './SecondsCounter'
+import PropTypes from 'prop-types'
+
+let offset = null, interval = null
+export default class Timer extends Component {
+    static get propTypes() {
+        return {
+            options: PropTypes.object
+        }
+    }
+
+    constructor(props) {
+        super(props)
+        this.onUpdate = props.onUpdate
+        this.state = {clock: 0, time: ''}
+        this.time = new Date()
+    }
+
+    componentDidMount() {
+        this.play()
+
+    }
+
+    componentWillUnmount() {
+        this.pause()
+    }
+
+    pause() {
+        if (interval) {
+            clearInterval(interval)
+            interval = null
+        }
+    }
+
+    play() {
+        if (!interval) {
+            offset = Date.now()
+            interval = setInterval(this.update.bind(this), this.props.options.delay)
+        }
+    }
+
+    clickDifference() {
+        let difference = new Date()
+        difference = difference - this.time
+        this.time = new Date()
+        return difference
+    }
+
+    reset() {
+        let clockReset = 0
+        this.setState({clock: clockReset})
+        let time = SecondsCounter(clockReset / 1000)
+        this.setState({time: time})
+    }
+
+    update() {
+        let clock = this.state.clock
+        clock += this.calculateOffset()
+        this.setState({clock: clock})
+        let time = SecondsCounter(clock / 1000)
+        this.setState({time: time})
+        this.onUpdate({clock})
+    }
+
+    calculateOffset() {
+        let now = Date.now()
+        let newOffset = now - offset
+        offset = now
+        return newOffset
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <h4 style={{width: "100%"}} >Timer: {this.state.time} {this.props.prefix}</h4>
+            </div>
+        )
+    }
+}
